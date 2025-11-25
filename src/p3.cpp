f3307f6dda0e7445f7da8e8e2b8d1e288fbd1a4d
hankmg14-afk <hank.mg14@nycu.edu.tw>
2025-11-25 14:36:31 +0800

    Update p3.cpp

--- src/p3.cpp
+++ src/p3.cpp
@@ -3,8 +3,27 @@
 #include <iostream>
 using namespace std;
 
+bool isPrime(int n){
+    int i ;
+    for(i=2;i<n;i++){
+        if(n%i==0){
+            return false;
+            break;
+        }else if (n-1==i){
+            return true;
+            break;
+        }
+    }
+}
 int main() {
-    
+    int n, i ;
+    cin>>n;
+    for(i=1;i<=n/2;i++){
+        if(isPrime(i)==1&&isPrime(n-i)==1){
+            break ;
+        }
+    }
+    cout<<i<<" "<<n-i<<endl;
     
     return 0;
 }
